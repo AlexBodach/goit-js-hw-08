2b695fae12008b948c5b86e18f7326097bd6bfca
Alex andr Bodachevskiy <alex.bodachevskiy@gmail.com>
2022-01-03 21:27:51 +0200

    upd 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'
 
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+    player.on('timeupdate', throttle(function() {
+        player.getCurrentTime().then(function(sec) {
+            console.log(sec)
+            localStorage.setItem("videoplayer-current-time", sec)
+        })
+    }, 1000),)
+ 
+
+    player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(sec) {
+        // seconds = the actual time that the player seeked to
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                break;
+    
+            default:
+                // some other error occurred
+                break;
+        }
+    }); 
