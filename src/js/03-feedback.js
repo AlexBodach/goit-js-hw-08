b1365c5f07581ac54172c1983aaec9ecd79993a2
Alex andr Bodachevskiy <alex.bodachevskiy@gmail.com>
2022-01-05 12:49:24 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,5 +1,7 @@
 import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
 const refs = {
     textAreaForm: document.querySelector('.feedback-form'),
     textEmail: document.querySelector('.email'),
@@ -23,19 +25,18 @@ function inputSaved (evt) {
     if (evt.target.name === 'message'){
         textInForm.message = evt.target.value;
     }
-   localStorage.setItem('feedback-form-state', 
-   JSON.stringify(textInForm))
+   localStorage.setItem(STORAGE_KEY, JSON.stringify(textInForm))
 }
 
 function formSubmit (evt) {
    evt.preventDefault();
    console.log(textInForm)
    evt.target.reset();
-   localStorage.removeItem('feedback-form-state') 
+   localStorage.removeItem(STORAGE_KEY) 
 }
 
 function populateTextArea () {
-    const savedMessage = JSON.parse(localStorage.getItem('feedback-form-state'));
+    const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
         if(savedMessage) {
             refs.textEmail.value = savedMessage.email;
             refs.textMessage.value = savedMessage.message;
