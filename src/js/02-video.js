8abb98d08b7785b9a44027352872601fec17e3ad
Alex andr Bodachevskiy <alex.bodachevskiy@gmail.com>
2022-01-05 12:37:07 +0200

    upd 3 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,12 @@
 import Player from '@vimeo/player';
-import throttle from 'lodash.throttle'
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
     const player = new Player(iframe);
 
     player.on('timeupdate', throttle(function() {
         player.getCurrentTime().then(function(sec) {
-            console.log(sec)
-            localStorage.setItem("videoplayer-current-time", sec)
+                    localStorage.setItem("videoplayer-current-time", sec)
         })
     }, 1000),)
  
@@ -17,11 +16,9 @@ const iframe = document.querySelector('iframe');
     }).catch(function(error) {
         switch (error.name) {
             case 'RangeError':
-                // the time was less than 0 or greater than the video’s duration
                 break;
     
             default:
-                // some other error occurred
                 break;
         }
     }); 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+    textAreaForm: document.querySelector('.feedback-form'),
+    textEmail: document.querySelector('.email'),
+    textMessage: document.querySelector('.message')   
+};
+
+refs.textAreaForm.addEventListener('input', throttle(inputSaved, 1000));
+refs.textAreaForm.addEventListener('submit', formSubmit);
+
+populateTextArea ();
+const textInForm = {
+   email:'',
+   message:''
+};   
+
+function inputSaved (evt) { 
+    if (evt.target.name === 'email') {
+        textInForm.email = evt.target.value;
+    } 
+    
+    if (evt.target.name === 'message'){
+        textInForm.message = evt.target.value;
+    }
+   localStorage.setItem('feedback-form-state', 
+   JSON.stringify(textInForm))
+}
+
+function formSubmit (evt) {
+   evt.preventDefault();
+   console.log(textInForm)
+   evt.target.reset();
+   localStorage.removeItem('feedback-form-state') 
+}
+
+function populateTextArea () {
+    const savedMessage = JSON.parse(localStorage.getItem('feedback-form-state'));
+        if(savedMessage) {
+            refs.textEmail.value = savedMessage.email;
+            refs.textMessage.value = savedMessage.message;
+    }
+}  
